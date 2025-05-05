98767f3f2691c89cf003df1521e6d83a8e756da6
Kirill Ivanchin <chvik1@yandex.ru>
2025-05-06 01:17:08 +0500

    main: add main logic

--- main.go
+++ main.go
@@ -1,15 +1,203 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
 )
+
+var (
+	config      Config
+	competitors = make(map[int]*Competitor)
+	timeLayout  = "15:04:05.000"
+)
+
+func formatDuration(d time.Duration) string {
+	ms := d.Milliseconds() % 1000
+	sec := int(d.Seconds()) % 60
+	mins := int(d.Minutes()) % 60
+	hour := int(d.Hours())
+	return fmt.Sprintf("%02d:%02d:%02d.%03d", hour, mins, sec, ms)
+}
+
+func parseConfig(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func logEvent(timeStr string, text string) {
+	fmt.Printf("[%s] %s\n", timeStr, text)
+}
+
+func processEvent(event string) {
+	re := regexp.MustCompile(`\[(.*?)\] (\d+) (\d+)(?: (.*))?`)
+	matches := re.FindStringSubmatch(event)
+
+	if len(matches) < 4 {
+		return
+	}
+
+	timeStr, eventIDStr, competitorIDStr := matches[1], matches[2], matches[3]
+
+	extra := ""
+	if len(matches) > 4 {
+		extra = matches[4]
+	}
+
+	//fmt.Println(timeStr, eventIDStr, competitorIDStr, extra)
+
+	eventID, _ := strconv.Atoi(eventIDStr)
+	competitorID, _ := strconv.Atoi(competitorIDStr)
+	eventTime, _ := time.Parse(timeLayout, timeStr)
+
+	c, ok := competitors[competitorID]
+	if !ok {
+		c = &Competitor{ID: competitorID}
+		competitors[competitorID] = c
+	}
+
+	switch eventID {
+	case 1:
+		c.Registered = true
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) registered", competitorID))
+	case 2:
+		c.StartTimePlanned, _ = time.Parse(timeLayout, extra)
+
+		logEvent(timeStr, fmt.Sprintf("The start time for the competitor(%d) was set by a draw to %s", competitorID, extra))
+	case 3:
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) is on the start line", competitorID))
+	case 4:
+		c.ActualStartTime = eventTime
+		c.CurrentLapStart = eventTime
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) has started", competitorID))
+	case 5:
+		if len(extra) == 0 {
+			log.Fatal("5. firing range: wrong format of extra field")
+		}
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) is on the firing range(%s)", competitorID, extra))
+	case 6:
+		c.Hits++
+		c.Shots++
+
+		target := extra
+
+		logEvent(timeStr, fmt.Sprintf("The target(%s) has been hit by competitor(%d)", target, competitorID))
+	case 7:
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) left the firing range", competitorID))
+	case 8:
+		c.PenaltyStart = eventTime
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) entered the penalty laps", competitorID))
+	case 9:
+		penaltyDuration := eventTime.Sub(c.PenaltyStart)
+		c.PenaltyTime += penaltyDuration
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) left the penalty laps", competitorID))
+	case 10:
+		lapDuration := eventTime.Sub(c.CurrentLapStart)
+		c.LapTimes = append(c.LapTimes, lapDuration)
+		c.CurrentLapStart = eventTime
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) ended the main lap", competitorID))
+
+		if len(c.LapTimes) == config.Laps {
+			c.Finished = true
+
+			logEvent(eventTime.Format(timeLayout), fmt.Sprintf("The competitor(%d) has finished", competitorID))
+		}
+	case 11:
+		c.NotFinished = true
+
+		logEvent(timeStr, fmt.Sprintf("The competitor(%d) can`t continue: %s", competitorID, extra))
+	default:
+		log.Printf("Unhandled event: %d", eventID)
+	}
+}
+
+func generateReport() {
+	fmt.Println()
+
+	var results []Result
+	for _, c := range competitors {
+		status := ""
+		var total time.Duration
+
+		if !c.Registered {
+			continue
+		}
+
+		if c.ActualStartTime.IsZero() {
+			status = "[NotStarted]"
+		} else if c.NotFinished {
+			status = "[NotFinished]"
+		} else {
+			total = c.CurrentLapStart.Sub(c.StartTimePlanned)
+		}
+
+		results = append(results, Result{CompetitorID: c.ID, TotalTime: total, Competitor: c, Status: status})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].CompetitorID < results[j].CompetitorID
+	})
+
+	for _, r := range results {
+		fmt.Printf("%s %d [", r.Status, r.CompetitorID)
+		for _, lap := range r.Competitor.LapTimes {
+			speed := float64(config.LapLen) / lap.Seconds()
+
+			fmt.Printf("{%s, %.3f}", formatDuration(lap), speed)
+		}
+
+		for i := 0; i < config.Laps-len(r.Competitor.LapTimes); i++ {
+			fmt.Print(" {,}")
+		}
+
+		fmt.Print("] ")
+
+		speedPenalty := 0.0
+		if r.Competitor.PenaltyTime > 0 {
+			speedPenalty = float64(config.PenaltyLen) / (r.Competitor.PenaltyTime.Seconds() / float64(r.Competitor.Hits))
+		}
+
+		fmt.Printf("{%s, %.3f} %d/5\n",
+			formatDuration(r.Competitor.PenaltyTime), speedPenalty,
+			r.Competitor.Hits)
+	}
+}
 
 func main() {
 	if len(os.Args) < 3 {
 		log.Fatal("run app by: go run main.go <config.json> <events.txt>")
 	}
 
-	fmt.Println(os.Args)
+	parseConfig(os.Args[1])
+
+	file, err := os.Open(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		processEvent(scanner.Text())
+	}
+
+	generateReport()
 }
